0ddca6e073174c5ec3a1942e82c9916c67cee5e0
unknown <jameskagunga2022@gmail.com>
2024-06-12 17:08:59 +0300

    structured the code to use controllers

---
+++ Backend/routes/parcel.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const {createParcel, getAllParcels, updateParcel, getOneParcel, getUserParcel, deleteParcel} = require("../controllers/parcel")
+
+// ADD PARCEL 
+router.post("/", createParcel)
+
+// GET ALL PARCELS
+
+router.get("/", getAllParcels)
+
+
+// UPDATE PARCEL
+router.put("/:id", updateParcel)
+
+// GET ONE PARCEL
+
+router.get("/find/:id", getOneParcel )
+
+// GET USERS PARCELS
+
+router.post("/me", getUserParcel)
+
+// DELETE PARCEL 
+
+router.delete("/:id", deleteParcel)
+
+module.exports = router;
