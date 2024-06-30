764f5d8cb04b5fe03d7910d04414c7006d78704a
unknown <jameskagunga2022@gmail.com>
2024-06-30 13:18:02 +0300

    started intergration with our backend

--- Backend/routes/parcel.js
+++ Backend/routes/parcel.js
@@ -1,29 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const {createParcel, getAllParcels, updateParcel, getOneParcel, getUserParcel, deleteParcel} = require("../controllers/parcel");
-const { verifyToken, verifyTokenAndAuthorization } = require("../middlewares/verifyToken");
+const {
+  createParcel,
+  getAllParcels,
+  updateParcel,
+  getOneParcel,
+  getUserParcel,
+  deleteParcel,
+} = require("../controllers/parcel");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+} = require("../middlewares/verifyToken");
 
-// ADD PARCEL 
-router.post("/",verifyToken,createParcel)
+// ADD PARCEL
+router.post("/", createParcel);
 
 // GET ALL PARCELS
 
-router.get("/", getAllParcels)
-
+router.get("/", getAllParcels);
 
 // UPDATE PARCEL
-router.put("/:id", updateParcel)
+router.put("/:id", updateParcel);
 
 // GET ONE PARCEL
 
-router.get("/find/:id", getOneParcel )
+router.get("/find/:id", getOneParcel);
 
 // GET USERS PARCELS
 
-router.post("/me", getUserParcel)
+router.post("/me", getUserParcel);
 
-// DELETE PARCEL 
+// DELETE PARCEL
 
-router.delete("/:id", deleteParcel)
+router.delete("/:id", deleteParcel);
 
 module.exports = router;
